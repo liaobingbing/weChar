f0a6f8972984fdcea4628fe31d2bcd48967bbece
hzm <hzm009@qq.com>
2018-04-26 13:16:32 +0800

    更新 Admin 模块,完成小程序列表 banner图模块

--- Application/Admin/Controller/AppController.class.php
+++ Application/Admin/Controller/AppController.class.php
@@ -38,7 +38,7 @@ class AppController extends AdminController
         $this->display();
     }
 
-    // 小程序添加页面
+    // 小程序更新页面
     public function app_edit()
     {
         $id = I('id');
@@ -136,6 +136,151 @@ class AppController extends AdminController
         $id = I('id');
 
         if (M('AppList')->delete($id)){
+            $result = array( 'code'=>200 ,'msg' => '删除成功!');
+        }
+
+        $this->ajaxReturn($result);
+    }
+
+    // 小程序Banner列表
+    public function app_banner_list()
+    {
+        // 类型
+        $type = M('AppList')->select();
+        $this->assign('appList',$type);
+        // 列表
+        $list = M('AppListBanner')->order('sort')->select();
+        $this->assign('list',$list);
+        $count = M('AppListBanner')->count();
+        $this->assign('count',$count);
+
+        $this->display();
+    }
+
+    // 小程序Banner添加页面
+    public function app_banner_add()
+    {
+        // 小程序列表
+        $list = M('AppList')->select();
+        $this->assign('list',$list);
+        $this->display();
+    }
+
+    // 小程序Banner更新页面
+    public function app_banner_edit()
+    {
+        $id = I('id');
+
+        $banner = M('AppListBanner')->find($id);
+        $app = M('AppList')->where(array('id'=>$banner['app_id']))->field('id,name')->find();
+        $this->assign('banner',$banner);
+        $this->assign('app',$app);
+
+        $this->display();
+    }
+
+    // 小程序Banner添加操作
+    public function do_banner_add()
+    {
+        $request = I('');
+        $banner = $_FILES['banner'];
+        $banner =  $this->qiniu_upload($banner,'App/Banner/');
+
+        $result = array('code' => 400, 'msg' => '添加失败');
+
+        if($banner){
+            $data['banner'] = $banner;
+
+            $app = M('AppList')->find($request['app_id']);
+
+            if( $app ){
+                $data['app_id']  =    $app['id'];
+                $data['sort']    =    $request['sort'];
+                $data['status']  =    $request['status'];
+                $data['add_time']=   time();
+
+                // 验证规则
+                $rules = array(
+                    array('app_id','','该小程序已经存在Banner图！',0,'unique',1), // 在新增的时候验证 appid 字段是否唯一
+                );
+                $AppListBanner = M('AppListBanner');
+                // 自动验证
+                if($AppListBanner->validate($rules)->create($data)){
+                    if( $AppListBanner->add()){
+                        $result = array('code'=>200,'msg'=>'添加成功');
+                    }
+                }else{
+                    $result = array('code'=>400,'msg'=>$AppListBanner->getError());
+                }
+            }else{
+                $result = array('code'=>400,'msg' => '请先添加小程序!');
+            }
+
+        }else{
+            $result = array('code' => 400, 'msg' => '图片上传失败!');
+        }
+
+        $this->ajaxReturn($result);
+    }
+
+    // 小程序Banner更新操作
+    public function do_banner_edit()
+    {
+        $result = array('code' => 400, 'msg' => '修改失败');
+        $request = I('');
+
+        if($_FILES['banner']){
+            $banner =  $this->qiniu_upload($_FILES['banner'],'App/Logo/');
+            if($banner) $data['banner']  =  $banner;
+        }
+
+        $app = M('AppListBanner')->find($request['id']);
+
+        if( $app ){
+            $data['id']  =    $app['id'];
+            $data['sort']    =    $request['sort'];
+            $data['status']  =    $request['status'];
+
+            if(M('AppListBanner')->save($data)){
+                $result = array('code' => 200, 'msg' => '修改成功!');
+            }
+
+        }else{
+            $result = array('code'=>400,'msg' => '请先添加小程序!');
+        }
+
+
+
+        $this->ajaxReturn($result);
+    }
+
+    // 小程序Banner状态变更
+    public function do_banner_status()
+    {
+        $id   = I('id');
+        $result = array('code' => 400, 'msg' => '变更失败');
+        $app = M('AppListBanner')->find($id);
+
+        if($app['status'] == 0){
+            $app['status'] = 1;
+        }else{
+            $app['status'] = 0;
+        }
+
+        if(M('AppListBanner')->save($app)){
+            $result = array('code' => 200, 'msg' => '变更成功');
+        }
+
+        $this->ajaxReturn($result);
+    }
+
+    // 小程序Banner删除操作
+    public function app_banner_del()
+    {
+        $result = array( 'code'=>400 ,'msg' => '删除失败');
+        $id = I('id');
+
+        if (M('AppListBanner')->delete($id)){
             $result = array( 'code'=>200 ,'msg' => '删除成功!');
         }
 
