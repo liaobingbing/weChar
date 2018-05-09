24fa5729315ae343e9c13e780b302ac32dd7b7ea
liaobingbing <1277936039@qq.com>
2018-05-09 18:26:21 +0800

    修改找色块登录方式

--- Application/FindColor/Controller/LoginController.class.php
+++ Application/FindColor/Controller/LoginController.class.php
@@ -10,6 +10,7 @@ namespace FindColor\Controller;
 
 
 use Common\Controller\ApiLoginController;
+use FindColor\Model\QuestionsModel;
 use FindColor\Model\UserGameModel;
 use FindColor\Model\UsersModel;
 
@@ -26,27 +27,30 @@ class LoginController extends ApiLoginController
     public function login(){
         $userdao=new UsersModel();
         $code=I('post.code');
-        $encryptedData=I('post.encryptedData');
-        $iv=I('post.iv');
-        $login_data=$this->get_weixin($code,$encryptedData,$iv);
-        if($login_data['code']!=400){
-            $openid = $login_data['openId'];
+        $userInfo=I('post.userInfo');//获取前台传送的用户信息
+        $userInfo=str_replace("&quot;","\"",$userInfo);
+        $userInfo=json_decode($userInfo,true);
+        $login_data=$this->test_weixin($code);
+        if($login_data['code']!=400&&$userInfo){
+            $session_key = $login_data['session_key'];
+            session('wx_session_key',$session_key);
+            $openid = $login_data['openid'];
             $user = $userdao->findByOpenid($openid);
             if (!$user) {
                 $user_data['openid'] = $openid;
-                $user_data['unionid'] = $login_data['unionId'];
-                $user_data['gender'] = $login_data['gender'];
-                $user_data['city'] = $login_data['city'];
+                $user_data['unionid'] = $userInfo['unionId'];
+                $user_data['gender'] = $userInfo['gender'];
+                $user_data['city'] = $userInfo['city'];
                 $user_data['login_time'] = time();
-                $user_data['province'] = $login_data['province'];
-                $user_data['country'] = $login_data['country'];
-                $user_data['avatar_url'] =  str_replace('/0','/132',$login_data['avatarUrl'] );
-                $user_data['name'] = $login_data['nickName'];
+                $user_data['province'] = $userInfo['province'];
+                $user_data['country'] = $userInfo['country'];
+                $user_data['avatar_url'] =  str_replace('/0','/132',$userInfo['avatarUrl'] );
+                $user_data['name'] = $userInfo['nickName'];
                 $uid = M('users')->data($user_data)->add();
                 $user_game['uid']=$uid;
-                $user_game['nickname']=$login_data['nickName'];
+                $user_game['nickname']=$userInfo['nickName'];
                 $user_game['login_time'] = time();
-                $user_game['avatar_url']=str_replace('/0','/132',$login_data['avatarUrl'] );
+                $user_game['avatar_url']=str_replace('/0','/132',$userInfo['avatarUrl'] );
                 M('user_game')->add($user_game);
             }else{
                 if($user['status']==0) {
@@ -55,11 +59,11 @@ class LoginController extends ApiLoginController
                     $data['data']['user_id']=$user['id'];
                     $this->ajaxReturn($data,'JSON');
                 }
-                if($user['login_time']<strtotime(date("Y-m-d"),time())){
+                /*if($user['login_time']<strtotime(date("Y-m-d"),time())){
                     M('users')->where('id='.$user['id'])->setField("avatar_url", str_replace('/0','/132',$login_data['avatarUrl']));
                     M('user_game')->where('uid='.$user['id'])->setField("avatar_url", str_replace('/0','/132',$login_data['avatarUrl']));
 
-                }
+                }*/
                 M('users')->where('id='.$user['id'])->setField("login_time",time());
                 $uid=$user['id'];
             }
@@ -140,11 +144,106 @@ class LoginController extends ApiLoginController
             echo '登录成功';
         }
     }
+// 获取题目
+    public function get_question(){
+        $layer = I('layer',1);
+        if($layer == 1){
+            session('questions',null);
+            $Questions = new QuestionsModel();
+            $questions = $Questions->get_rand_questions();
+            // print_r($questions);die;
+            session('questions',$questions);//获取所有的题目并且放在session中
+        }
 
+        $questions = session('questions');
+
+        $option = array_shift($questions);
+        //session('questions',$questions);
+        //print_r($questions);die;
+        if( !$option ){
+            $this->ajaxReturn(array('code' => 400, 'msg' => '获取失败'));
+        }
+
+
+        if( $layer <= 2 ){
+            $i = 4;//4个色块
+            $j = 1;//色块大小100%
+        }else if( $layer <= 5 ){
+            $i = 9;//9块色块
+            $j = 0.65;//色块大小65%
+        }else if( $layer <= 9 ){
+            $i = 16;
+            $j = 0.48;
+        }else if( $layer <= 14 ){
+            $i = 25;
+            $j = 0.38;
+        }else if( $layer <= 20 ){
+            $i = 36;
+            $j = 0.32;
+        }else if( $layer <= 27 ){
+            $i = 49;
+            $j = 0.28;
+        }else if( $layer <= 35 ){
+            $i = 64;
+            $j = 0.23;
+        }else if( $layer <= 45 ){
+            $i = 81;
+            $j = 0.21;
+        }else{
+            $this->ajaxReturn(array('code' => 400, 'msg' => 'layer不能超过44'));
+        }
+
+        $arr = array();
+
+        if($option['answer'] != 'option_1'){
+            for($a = 0; $a < $i - 1; $a++){
+                $arr[$a]['text']    = $option['option_1'];
+                $arr[$a]['percent'] = $j;
+            }
+            if( $layer == 44 ){
+                $arr[$i - 1]['text'] = $option['option_1'];
+                $arr[$i - 1]['percent'] = $j;
+            }else{
+                $arr[$i - 1]['text'] = $option['option_2'];
+                $arr[$i - 1]['percent'] = $j;
+            }
+
+            $answer = $option['option_2'];
+        }else{
+            for($a = 0; $a < $i - 1; $a++){
+                $arr[$a]['text']    = $option['option_2'];
+                $arr[$a]['percent'] = $j;
+            }
+            if( $layer == 44 ){
+                $arr[$i - 1]['text'] = $option['option_2'];
+                $arr[$i - 1]['percent'] = $j;
+            }else{
+                $arr[$i - 1]['text'] = $option['option_1'];
+                $arr[$i - 1]['percent'] = $j;
+            }
+            $answer = $option['option_1'];
+        }
+
+        shuffle($arr);
+
+        $result['code'] = 200;
+        $result['msg']  = '获取成功';
+        $result['data']['words'] = $arr;
+        $result['data']['answer'] = $answer;
+        $result['data']['next_layer'] = $layer+1;
+
+
+        $this->ajaxReturn($result);
+    }
     // 测试 - 登录退出
     public function test_logout(){
         session(null);
         echo '退出成功';
+    }
+
+    public function set_session(){
+        session("user_id",8);
+
     }
 
 
