56479270a14bb88504310f32b65116b786b928b8
liaobingbing <1277936039@qq.com>
2018-05-10 10:34:25 +0800

    修改找字，找色块登录方式

--- Application/Confuse/Controller/LoginController.class.php
+++ Application/Confuse/Controller/LoginController.class.php
@@ -83,11 +83,9 @@ class LoginController extends  ApiLoginController
             foreach($user_info as $k=>$v){
                 $user_info[$k]['ranking']=$k+1;
             }
-            $sql1="SELECT avatarUrl,gt_number,nickname FROM confuse_test_game order by gt_number desc limit 3";
-            $data1=M()->query($sql1);
-            $sql2="SELECT avatarUrl,gt_number,nickname FROM confuse_test_game WHERE id >= ((SELECT MAX(id) FROM confuse_test_game)-(SELECT MIN(id) FROM confuse_test_game)) * RAND() + (SELECT MIN(id) FROM confuse_test_game)  order by  gt_number desc LIMIT 1,5";
-            $data2=M()->query($sql2);
-            $user_info2=array_merge($data1,$data2);
+            $sql2="SELECT avatar_url as avatarUrl ,get_number as gt_number ,nickname FROM method_user_game   order by  gt_number desc LIMIT 0,8";
+            $user_info2=M()->query($sql2);
+            //$user_info2=array_merge($data1,$data2);
             foreach($user_info2 as $k=>$v){
                 $user_info2[$k]['ranking']=$k+1;
             }
@@ -113,11 +111,8 @@ class LoginController extends  ApiLoginController
         $user_info = S('c_intelligence_top');
         if(!$user_info){
             //SELECT avatarUrl,gt_number as number,nickname FROM method_test_game WHERE id >= ((SELECT MAX(id) FROM method_test_game)-(SELECT MIN(id) FROM method_test_game)) * RAND() + (SELECT MIN(id) FROM method_test_game)  order by  number desc LIMIT 5;
-            $sql1="SELECT avatarUrl,gt_number,nickname FROM confuse_test_game order by gt_number desc limit 3";
-            $data1=M()->query($sql1);
-            $sql2="SELECT avatarUrl,gt_number,nickname FROM confuse_test_game WHERE id >= ((SELECT MAX(id) FROM confuse_test_game)-(SELECT MIN(id) FROM confuse_test_game)) * RAND() + (SELECT MIN(id) FROM confuse_test_game)  order by  gt_number desc LIMIT 1,5";
-            $data2=M()->query($sql2);
-            $user_info=array_merge($data1,$data2);
+            $sql2="SELECT avatar_url as avatarUrl ,get_number as gt_number ,nickname FROM method_user_game   order by  gt_number desc LIMIT 0,8";
+            $user_info=M()->query($sql2);
             foreach($user_info as $k=>$v){
                 $user_info[$k]['ranking']=$k+1;
             }
